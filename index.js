e877679dfcbc6cdcc00b40e536a532e507ee9254
Olivier Rousseau-Guyot <zeachco@gmail.com>
2015-03-26 11:38:28 -0400

    added delay mocking, function extensions and colors fix

--- index.js
+++ index.js
@@ -5,19 +5,36 @@ var url = require('url');
 
 var config = require('./config');
 
-if(config.entryPort && config.entryPort < 1024){
+if (config.entryPort && config.entryPort < 1024) {
   var uid = parseInt(process.env.SUDO_UID);
-  if (!uid){
-    console.log(('Port is '+config.entryPort + ', you may need to run as a superuser to open that port').red)
+  if (!uid) {
+    console.log(('Port is ' + config.entryPort + ',\nyou may need to run as a superuser to open ports below :1024\n\n').red)
   }
 }
 
 app.use(function(req, res, next) {
-  var path = url.parse(req.url).path.split('?')[0];
   try {
-    res.json(require('.' + path + '.' + req.method));
-    if (config.showMocks)
-      console.log('MOCKING'.green, req.method, path.green);
+
+    var path = url.parse(req.url).path.split('?')[0];
+    var mock = '.' + path + '.' + req.method.toLowerCase();
+    var response = require(mock);
+    if (typeof response === 'function') {
+      response = response(req, res);
+    }
+
+    var delay = Math.ceil(Math.random() * config.throttle / 2) + config.throttle / 2;
+    setTimeout(function() {
+      try {
+        res[typeof response === 'object' ? 'json' : 'end'](response);
+        if (config.showMocks) {
+          console.log('MOCKING'.green, req.method, path.green, ('mocking latency of ' + delay + 'ms').gray);
+        }
+      } catch (e) {
+        console.error(e);
+        next();
+      }
+    }, delay);
+
   } catch (e) {
     next();
   }
@@ -29,9 +46,9 @@ app.use(proxy(backendAddress, {
     return require('url').parse(req.url).path;
   },
   intercept: function(data, req, res, callback) {
-    var path = url.parse(req.url).path.split('?')[0];
+    var path = url.parse(req.url).path;
     if (!data.length) {
-      console.log('ERROR'.red, req.method, path.red, 'does not exist');
+      console.log('PROXY'.red, req.method, path.red, 'does not exist');
     } else if (config.showServes) {
       console.log('PROXY'.yellow, req.method, path.green);
     }
