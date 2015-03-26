20861c989f026b1cf64f70d590432d00c52f324e
Olivier Rousseau-Guyot <zeachco@gmail.com>
2015-03-26 10:21:32 -0400

    added start script, sudo message and port 80 config

--- index.js
+++ index.js
@@ -5,32 +5,36 @@ var url = require('url');
 
 var config = require('./config');
 
+if(config.entryPort && config.entryPort < 1024){
+  var uid = parseInt(process.env.SUDO_UID);
+  if (!uid){
+    console.log(('Port is '+config.entryPort + ', you may need to run as a superuser to open that port').red)
+  }
+}
+
 app.use(function(req, res, next) {
   var path = url.parse(req.url).path.split('?')[0];
   try {
     res.json(require('.' + path + '.' + req.method));
     if (config.showMocks)
-      console.log('mocking', req.method, path.green);
+      console.log('MOCKING'.green, req.method, path.green);
   } catch (e) {
     next();
   }
 });
 
-var backend = (config.host || '127.0.0.1') + ':' + (config.port || 8080);
-
-app.use(proxy(backend, {
+var backendAddress = (config.host || '127.0.0.1') + ':' + (config.port || 8080);
+app.use(proxy(backendAddress, {
   forwardPath: function(req, res) {
     return require('url').parse(req.url).path;
   },
   intercept: function(data, req, res, callback) {
     var path = url.parse(req.url).path.split('?')[0];
-    
     if (!data.length) {
       console.log('ERROR'.red, req.method, path.red, 'does not exist');
     } else if (config.showServes) {
-      console.log('serving', req.method, path.yellow);
+      console.log('PROXY'.yellow, req.method, path.green);
     }
-
     callback(null, data);
   }
 }));
