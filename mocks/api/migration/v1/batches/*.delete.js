97c570754863d6c50ff8e54c857f2e0b64184f70
Olivier Rousseau-Guyot <zeachco@gmail.com>
2015-03-31 17:34:02 -0400

    cleanup

--- mocks/api/migration/v1/batches/*.delete.js
+++ mocks/api/migration/v1/batches/*.delete.js
@@ -1,15 +1,19 @@
 module.exports = {
   _mock: function(req, res, next) {
+
     var destroyed = [];
-    console.log(req.wildcard);
+
     req.session.batches.forEach(function(d, i) {
-      console.log(d.id, req.wildcard);
+
       if ('' + d.id === '' + req.wildcard) {
         destroyed.push(req.session.batches.splice(i, 1));
       }
+
     });
+
     return {
       destroyed: destroyed
     };
+
   }
 };
