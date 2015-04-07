53634856cbfc4baea1a817a5b3bb28c0d063eab3
Olivier Rousseau-Guyot <zeachco@gmail.com>
2015-04-06 22:57:06 -0400

    added file

---
+++ mocks/api/migration/v1/batches/*.post.js
@@ -0,0 +1,20 @@
+module.exports = {
+  _mock: function(req, res, next) {
+
+    var i = 0;
+    var d = null;
+    while (d = req.session.batches[i++]) {
+
+      if ('' + d.id === '' + req.wildcard) {
+
+        for (var n in req.body) {
+          d[n] = req.body[n];
+        }
+
+        return d;
+      }
+
+    }
+
+  }
+};
